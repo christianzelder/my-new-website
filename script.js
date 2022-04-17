ac8fe663f686abbcfd9a702ff08b6bc06d87a184
christianmaldonado <christian.zldr@gmail.com>
2022-04-17 19:02:49 -0400

    add script.js, connect to index.html, add style.css

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const spotify = document.getElementById('spotify');
+
+// 👇️ Change text color on mouseover
+spotify.addEventListener('mouseover', function handleMouseOver() {
+  spotify.style.color = 'red';
+});
+
